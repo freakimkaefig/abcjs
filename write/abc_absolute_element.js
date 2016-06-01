98efdd5680d4e3f8e45de63a5598dd3062df4841
Lukas Lamm <lukas.lamm89@gmail.com>
2016-06-01 21:56:48 +0200

    Change note highlight color

--- write/abc_absolute_element.js
+++ write/abc_absolute_element.js
@@ -240,7 +240,7 @@ ABCJS.write.AbsoluteElement.prototype.highlight = function (klass, color) {
 	if (klass === undefined)
 		klass = "note_selected";
 	if (color === undefined)
-		color = "#ff0000";
+		color = "#325D88";
 	this.setClass(klass, "", color);
 };
 
